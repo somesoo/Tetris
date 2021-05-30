fd2a0fe07f3cff7e6f326e7157471309c64f2654
Jonatan Kasperczak <jakasperczak@gmail.com>
2021-05-30 19:36:09 +0200

    class point

---
+++ point.h
@@ -0,0 +1,18 @@
+//
+// Created by johnny on 30.05.2021.
+//
+
+#ifndef TETRIS_POINT_H
+#define TETRIS_POINT_H
+
+
+class point {
+    int x;
+    int y;
+public:
+    int get_x(){return x;}
+    int get_y(){return y;}
+};
+
+
+#endif //TETRIS_POINT_H
