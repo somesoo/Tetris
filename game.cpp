4ee582f3b601343aaa1f19e15dfc69f4d8e8e448
Jonatan Kasperczak <jakasperczak@gmail.com>
2021-06-11 14:54:27 +0200

    more function bases

--- game.cpp
+++ game.cpp
@@ -1,5 +1,97 @@
-//
-// Created by johnny on 30.05.2021.
-//
-
+#include <ncurses.h>
+#include <time.h>
+#include <iostream>
+#include <thread>
 #include "game.h"
+#include "tetromino.h"
+
+
+game::game() {
+    board plansza;
+    start_game();
+    while(is_over()){
+    int mv = get_move();
+        move(mv);
+    refresh();}
+
+}
+int choose_random(int & current)
+{
+    srand((unsigned) time(NULL));
+    int newpiece=0;
+    newpiece=rand()%7+1;
+    while(newpiece==current)
+        newpiece=rand()%7+1;
+    printw("%d", newpiece);
+    current=newpiece;
+    return newpiece;
+}
+void game::new_falling_tetromino(int current_tetromino){
+    switch (choose_random(current_tetromino)) {
+        case 1:{
+            I_tetromino new_piece;
+            break;}
+        case 2:{
+            J_tetromino new_piece;
+            break;
+        }
+        case 3:{
+            L_tetromino new_piece;
+            break;
+        }
+        case 4:{
+            O_tetromino new_piece;
+            break;
+        }
+        case 5:{
+            S_tetromino new_piece;
+            break;
+        }
+        case 6:{
+            Z_tetromino new_piece;
+            break;
+        }
+        case 7:{
+            T_tetromino new_piece;
+            break;
+        }
+    }
+}
+
+int game::get_move()
+{
+    int key = getchar();
+    return key;
+}
+
+void game::move(int num)
+{
+//  right   279167 // 100
+//  left    279168 // 97
+//  down    279166 // 115
+//  rotate X 120
+//  space   32
+    switch (num) {
+        case 100:
+            printw("right\n");
+            break;
+        case 97:
+            printw("left\n");
+            break;
+        case 115:
+            printw("down\n");
+            break;
+        case 120:
+            printw("rotate\n");
+            break;
+        case 32:
+            printw("space\n");
+            break;
+        case 113:
+            game_over();
+            break;
+        default:
+            printw("wrong input");
+    }
+
+}
--- game.h
+++ game.h
@@ -1,7 +1,4 @@
-//
-// Created by johnny on 30.05.2021.
-//
-
+#include "board.h"
 #ifndef TETRIS_GAME_H
 #define TETRIS_GAME_H
 
@@ -11,13 +8,17 @@ class game {
     int score=0;
     bool isOver = false;
 public:
+    game();
     int get_score(){return score;}
     void check_collisions();
-    void new_falling_tetromino();
+    void new_falling_tetromino(int current_tetromino);
     void pause();
     void play();
     void start_game() {isOver= true;}
     bool is_over() const {return isOver;}
+    void game_over() {isOver=false;}
+    void move(int num);
+    int get_move();
 
 };
 
