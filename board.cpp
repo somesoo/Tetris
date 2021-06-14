d229f4b678461914e902df7c83905e83b36d8f5b
Jonatan Kasperczak <jakasperczak@gmail.com>
2021-06-14 02:42:14 +0200

    added boolean boarders

--- board.cpp
+++ board.cpp
@@ -1,25 +1,28 @@
 #include<ncurses.h>
 #include "board.h"
-#include "game.h"
+//#include "game.h"
 
 board::board() {
 
-    Board = new bool *[get_width()];
-    for (int i = 0; i < get_width(); ++i) {
-        Board[i] = new bool[get_height()];
-        for (int j = 0; j < get_height(); ++j) {
+    Board = new bool *[get_width()+2];
+    for (int i = 0; i < get_width()+2; ++i) {
+        Board[i] = new bool[get_height()+1];
+        for (int j = 0; j < get_height()+1; ++j) {
             Board[i][j] = false;
-            //if(j>17)
-             //   Board[i][j]= true;
+//            if(j>17)
+//                Board[i][j]= true;
         }
     }
+    for (int i = 0; i < get_width(); ++i)
+        Board[i][21]=true;
+        for(int j = 0;j<get_height(); j++){
+            Board[0][j]=true;
+            Board[11][j]=true;}
 }
 void board::draw_self(){
     for (int i = 0; i < get_height(); i++) {
         move(i+1, 6); // otherwise the box won't draw
         printw("X");
-        //for (int j = 0; j < get_width(); j++)
-        //printw(" ");
         move(i+1, get_width()+7);
         printw("X");
         }
