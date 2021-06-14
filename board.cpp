5712f9acf4881bc88ff968830b6439db21394a20
Jonatan Kasperczak <jakasperczak@gmail.com>
2021-06-14 14:36:47 +0200

    drawing boarders and occupied spaces

--- board.cpp
+++ board.cpp
@@ -14,12 +14,14 @@ board::board() {
         }
     }
     for (int i = 0; i < get_width(); ++i)
-        Board[i][21]=true;
+        Board[i][20]=true;
         for(int j = 0;j<get_height(); j++){
             Board[0][j]=true;
             Board[11][j]=true;}
 }
 void board::draw_self(){
+
+    //boundary
     for (int i = 0; i < get_height(); i++) {
         move(i+1, 6); // otherwise the box won't draw
         printw("X");
@@ -29,7 +31,13 @@ void board::draw_self(){
     move(21,6);
     for(int j = 0; j < get_width()+2; j++)
         printw("X");
-    //refresh();
+    //currentpieces
+    for(int i=1; i<get_width();i++)
+        for(int j=1;j<get_height();j++)
+            if(Board[i][j]) {
+                move(j+1, i+6);
+                printw("X");
+            }
 }
 board::~board(){
     for(int i=0;i<get_width();++i)
